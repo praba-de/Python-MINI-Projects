7a1fdcfab5b1c3a7e83e21b680956703af607d9a
Praba <prabalatha12k@gmail.com>
2026-03-14 09:42:06 +0530

    Quiz Game

---
+++ Quiz Game 1.py
@@ -0,0 +1,53 @@
+# Python Mini Projects
+
+## Quiz Game
+
+#A simple Python command-line quiz game.
+
+### Features
+#  Multiple computer-related questions
+# Score tracking
+# Percentage result
+
+### Run the project
+
+print("Welcome to my computer QUIZ !")
+
+playing = input(" Do you want to play ? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay ! Let's Play :) ")
+score = 0
+
+answer = input(" What does CPU stand for ? ")
+if answer.lower() == "central processing unit" :
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input(" What does GPU stand for ? ")
+if answer.lower() == "graphics processing unit" :
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input(" What does RAM stand for ? ")
+if answer.lower() == "random access memory" :
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input(" What does PSU stand for ? ")
+if answer.lower() == "power supply unit" :
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+print("You got " + str(score) +" questions correct ! ")
+print ("You got " + str((score/ 4)* 100 ) + "%.")
